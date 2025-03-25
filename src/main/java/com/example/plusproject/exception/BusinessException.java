bea2c9351e7afa8c6b883af03a2ca00dfcea494c
JoeMinKyung <mk981015@naver.com>
2025-03-26 03:22:49 +0900

    refactor: 파일 삭제

--- src/main/java/com/example/plusproject/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package com.example.plusproject.exception;
-
-public class BusinessException extends CustomException {
-
-  public BusinessException() { super(ErrorCode.BUSINESS_ERROR);}
-
-  public BusinessException(String message) { super(ErrorCode.BAD_REQUEST, message); }
-}
