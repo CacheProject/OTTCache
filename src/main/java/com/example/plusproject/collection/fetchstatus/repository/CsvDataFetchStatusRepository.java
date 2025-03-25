f0664286e32fc6f70402440dfe259c67d1ecf593
JoeMinKyung <mk981015@naver.com>
2025-03-25 22:35:18 +0900

    feat: findTopByOrderByUpdatedAtDesc 메서드 추가

--- src/main/java/com/example/plusproject/collection/fetchstatus/repository/CsvDataFetchStatusRepository.java
+++ src/main/java/com/example/plusproject/collection/fetchstatus/repository/CsvDataFetchStatusRepository.java
@@ -4,4 +4,6 @@ import com.example.plusproject.collection.fetchstatus.entity.CsvDataFetchStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CsvDataFetchStatusRepository extends JpaRepository<CsvDataFetchStatus, Long> {
+
+    CsvDataFetchStatus findTopByOrderByUpdatedAtDesc();  // 가장 최근에 업데이트된 레코드 찾기
 }
