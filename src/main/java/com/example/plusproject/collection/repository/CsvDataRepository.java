d08484f6f992c09b5737800f74dd2f01be01c4b2
JoeMinKyung <mk981015@naver.com>
2025-03-26 00:45:10 +0900

    refactor: 브랜치 파일 정리

--- src/main/java/com/example/plusproject/collection/repository/CsvDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.plusproject.collection.repository;
-
-import com.example.plusproject.collection.entity.CsvData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CsvDataRepository extends JpaRepository<CsvData, Long> {
-}
--- src/main/java/com/example/plusproject/openapi/repository/OpenApiRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.plusproject.openapi.repository;
-
-import com.example.plusproject.openapi.entity.OpenApi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OpenApiRepository extends JpaRepository<OpenApi, Long> {
-}
