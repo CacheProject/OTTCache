8c2ed75bbdbf1099d2768c8f31b5b327ff23fb2b
JoeMinKyung <mk981015@naver.com>
2025-03-27 02:01:43 +0900

    feat: ShoppingMallRepository 추가 및 main_product_category 칼럼에서 LIKE 조건으로 검색하고, Pageable을 사용하여 페이징 처리 메서드 추가

---
+++ src/main/java/com/example/cacheproject/shoppingmall/repository/ShoppingMallRepository.java
@@ -0,0 +1,14 @@
+package com.example.cacheproject.shoppingmall.repository;
+
+import com.example.cacheproject.shoppingmall.entity.ShoppingMall;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ShoppingMallRepository extends JpaRepository<ShoppingMall, Long> {
+
+    // main_product_category 칼럼에서 LIKE 조건으로 검색하고, Pageable을 사용하여 페이징 처리
+    Page<ShoppingMall> findByMainProductCategoryContainingIgnoreCase(String keyword, Pageable pageable);
+}
