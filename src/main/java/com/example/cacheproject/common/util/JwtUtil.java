0601184debd2e5ef96d4a89722c15b1e95e401fb
JoeMinKyung <mk981015@naver.com>
2025-03-27 16:13:40 +0900

    refactor: init

--- src/main/java/com/example/cacheproject/common/util/JwtUtil.java
+++ src/main/java/com/example/cacheproject/common/util/JwtUtil.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Slf4j(topic = "JwtUtil")
@@ -30,11 +31,8 @@ public class JwtUtil {
 
     @PostConstruct
     public void init() {
-        try {
-            key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
-        } catch (Exception e) {
-            throw new IllegalStateException("Failed to initialize JWT key", e);
-        }
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
     }
 
     /* Access Token 생성 */
