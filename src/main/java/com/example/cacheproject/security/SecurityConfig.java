28ee51796fdaddef4986e8f500d59d9ccc074b79
JoeMinKyung <mk981015@naver.com>
2025-03-27 23:11:05 +0900

    feat: permitAll() url 추가

--- src/main/java/com/example/cacheproject/security/SecurityConfig.java
+++ src/main/java/com/example/cacheproject/security/SecurityConfig.java
@@ -32,8 +32,10 @@ public class SecurityConfig {
                                 "/stores/**",
                                 "/api/v1/boards/search",
                                 "/api/v2/boards/search",
+                                "/api/v3/boards/search",
                                 "/api/v1/popular",
-                                "/api/v2/popular").permitAll()
+                                "/api/v2/popular",
+                                "/api/v3/popular" ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
