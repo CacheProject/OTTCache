02b6653d781fac50fa50b0aa0eadb2313b525758
JoeMinKyung <mk981015@naver.com>
2025-03-27 23:09:58 +0900

    feat: PageResponse 개선

--- src/main/java/com/example/cacheproject/common/response/PageResponse.java
+++ src/main/java/com/example/cacheproject/common/response/PageResponse.java
@@ -1,21 +1,34 @@
 package com.example.cacheproject.common.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Getter
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class PageResponse<T> implements Response<List<T>> {
+@Setter
+public class PageResponse<T> implements Response<List<T>>, Serializable {
+    private static final long serialVersionUID = 1L;
 
-    private final List<T> data;
-    private final int pageNumber;
-    private final int pageSize;
-    private final int totalPages;
-    private final long totalElements;
+    private List<T> data;
+    private int pageNumber;
+    private int pageSize;
+    private int totalPages;
+    private long totalElements;
 
-    public PageResponse(List<T> data, int pageNumber, int pageSize, int totalPages, long totalElements) {
+    // Jackson 역직렬화를 위한 생성자
+    @JsonCreator
+    public PageResponse(
+            @JsonProperty("data") List<T> data,
+            @JsonProperty("pageNumber") int pageNumber,
+            @JsonProperty("pageSize") int pageSize,
+            @JsonProperty("totalPages") int totalPages,
+            @JsonProperty("totalElements") long totalElements
+    ) {
         this.data = data;
         this.pageNumber = pageNumber;
         this.pageSize = pageSize;
@@ -23,9 +36,22 @@ public class PageResponse<T> implements Response<List<T>> {
         this.totalElements = totalElements;
     }
 
+    // 기본 생성자 (필요한 경우)
+    public PageResponse() {}
+
+    // Page로부터 생성하는 메서드
+    public static <T> Response<List<T>> fromPage(Page<T> pageData) {
+        return new PageResponse<>(
+                pageData.getContent(),
+                pageData.getNumber(),
+                pageData.getSize(),
+                pageData.getTotalPages(),
+                pageData.getTotalElements()
+        );
+    }
+
     @Override
     public List<T> getData() {
-        return data;
+        return this.data;
     }
 }
-
