2ec47d36dc5952cfc79fbf2deb689669507fc55a
이승민 <iseungmin@iseungmin-ui-MacBookPro.local>
2025-03-28 13:27:29 +0900

    fix: 충동해결

--- src/main/java/com/example/cacheproject/common/util/JwtUtil.java
+++ src/main/java/com/example/cacheproject/common/util/JwtUtil.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Slf4j(topic = "JwtUtil")
